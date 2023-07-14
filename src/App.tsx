fb6569113b68bb4f443af1e3acae4e274948947d
Eric Xie <ericxie1998@gmail.com>
2023-07-14 01:17:23 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./styles.css";
-import {ReactComponent as LogoSVG} from "./assets/logo.svg";
+import Hero from "./common/hero/Hero";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <LogoSVG />
+      <Hero />
     </div>
   );
 }
---
+++ src/common/hero/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import {ReactComponent as LogoSVG} from "@/assets/logo.svg";
+const Root = styled.section``
+
+const Hero = () => {
+    return(
+        <Root>
+            <LogoSVG />
+        </Root>
+    )
+}
+
+export default Hero;
